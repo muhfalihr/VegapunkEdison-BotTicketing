20b090ea87b1b7434d1d1464200fbdb8bfffff05
muhfalihr <muhammadfalihromadhoni@gmail.com>
2025-04-04 09:02:06 +0700

    ⚙ fix: (+ SQL)️

--- source/src/localization/queries.py
+++ source/src/localization/queries.py
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS ticket_messagesv2 (
     message TEXT NOT NULL,
     message_from ENUM('admin', 'user', 'handler') DEFAULT 'user',
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (ticket_id) REFERENCES ticketsv4(ticket_id) ON DELETE CASCADE,
+    FOREIGN KEY (ticket_id) REFERENCES ticketsv2(ticket_id) ON DELETE CASCADE,
     INDEX idx_ticket_id (ticket_id),
     INDEX idx_user_id (user_id),
     INDEX idx_timestamp (timestamp)
@@ -76,23 +76,23 @@ WHERE TABLE_SCHEMA = %s;
 """
 
 INSERT_USER_FOR_HANDLER: str = """
-INSERT IGNORE INTO handlers (user_id, username) VALUES (%s, %s)
+INSERT IGNORE INTO handlersv2 (user_id, username) VALUES (%s, %s)
 """
 
 DELETE_USER_FROM_HANDLER: str = """
-DELETE FROM handlers WHERE user_id = %s;
+DELETE FROM handlersv2 WHERE user_id = %s;
 """
 
 GET_ALL_HANDLERS: str = """
-SELECT * FROM handlers
+SELECT * FROM handlersv2
 """
 
 CHECK_USER_IS_HANDLER: str = """
-SELECT COUNT(*) FROM handlers WHERE user_id = %s
+SELECT COUNT(*) FROM handlersv2 WHERE user_id = %s
 """
 
 CREATE_TICKET: str = """
-INSERT INTO ticketsv4 (ticket_id, user_id, message_id, message_chat_id, username, userfullname, issue, created_at, status) 
+INSERT INTO ticketsv2 (ticket_id, user_id, message_id, message_chat_id, username, userfullname, issue, created_at, status) 
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
 """
 
@@ -108,12 +108,12 @@ WHERE id = %s
 """
 
 ADDED_TICKET_MESSAGE: str = """
-INSERT INTO ticket_messagesv5 (ticket_id, user_id, message_id, message_chat_id, username, userfullname, message, message_from, timestamp) 
+INSERT INTO ticket_messagesv2 (ticket_id, user_id, message_id, message_chat_id, username, userfullname, message, message_from, timestamp) 
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
 """
 
 GET_TICKET_BY_ID: str = """
-SELECT * FROM ticketsv4 WHERE ticket_id = %s
+SELECT * FROM ticketsv2 WHERE ticket_id = %s
 """
 
 GET_USER_DETAILS_BY_ID: str = """
@@ -121,47 +121,47 @@ SELECT * FROM usersv2 WHERE id = %s
 """
 
 GET_TICKET_MESSAGES: str = """
-SELECT COUNT(*) AS count FROM ticketsv4 
+SELECT COUNT(*) AS count FROM ticketsv2 
 WHERE ticket_id = %s AND user_id = %s
 """
 
 GET_ALL_TICKET_MESSAGES: str = """
 SELECT username, userfullname, message, timestamp 
-FROM ticket_messagesv5
+FROM ticket_messagesv2
 WHERE ticket_id = %s 
 ORDER BY timestamp ASC
 """
 
 CLOSED_TICKET: str = """
-UPDATE ticketsv4 
+UPDATE ticketsv2 
 SET status = 'closed', handler_id = %s, handler_username = %s, closed_at = %s 
 WHERE ticket_id = %s AND status = 'open'
 """
 
 GET_USER_TICKETS: str = """
 SELECT ticket_id, user_id, message_id, message_chat_id, issue, status, created_at, closed_at, handler_username 
-FROM ticketsv4 
+FROM ticketsv2 
 WHERE user_id = %s AND status = 'open'
 ORDER BY created_at DESC
 """
 
 GET_CLOSED_TICKETS_BY_TICKETID: str = """
 SELECT ticket_id, handler_username, closed_at 
-FROM ticketsv4 
+FROM ticketsv2 
 WHERE ticket_id = %s AND status = 'closed'
 ORDER BY closed_at DESC
 """
 
 GET_CLOSED_TICKETS: str = """
 SELECT ticket_id, issue, created_at, closed_at 
-FROM ticketsv4 
+FROM ticketsv2 
 WHERE user_id = %s AND status = 'closed'
 ORDER BY closed_at DESC
 """
 
 GET_HISTORY_HANDLER_TICKETS: str = """
 SELECT ticket_id, issue, status, created_at, closed_at, handler_username 
-FROM ticketsv4
+FROM ticketsv2
 WHERE handler_id = %s 
   AND DATE(closed_at) = CURDATE() 
 ORDER BY closed_at ASC
@@ -170,7 +170,7 @@ LIMIT 10;
 
 GET_OPENED_TICKETS: str = """
 SELECT ticket_id, username, message_id, message_chat_id, userfullname, created_at 
-FROM ticketsv4 
+FROM ticketsv2 
 WHERE status = 'open'
 ORDER BY created_at DESC
 """
