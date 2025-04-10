e1488e789b5ed6524ba4f8510f7a0889a742741f
muhfalihr <muhammadfalihromadhoni@gmail.com>
2025-04-10 09:28:10 +0700

    ✨ feat: ( + Fix Const Commands )

--- source/src/utility/const.py
+++ source/src/utility/const.py
@@ -1,6 +1,6 @@
-INVALID_MESSAGE_IN_USER: str = "/open,/close,/conversation,/history,/regist,/regist,/handlers"
+INVALID_MESSAGE_IN_USER: str = "/open,/close,/conversation,/history,/regist,/deregist,/handlers"
 BAD_WORDS: str = "jancok,bangsat,bajingan,cok,anjeng,kontol,ajg,ngentot,goblok"
 
 MESSAGE_PATTERN: str = "🎫 \*?Ticket\*? #([a-z0-9]+)\n\n🪪 \*?(.*?)\*? \(@([^)]+)\)\n⏰ `?([^`]+)`?\n\n📝 \*?Details :\*?\n([\s\S]+)"
 
-COMMANDS: str = "/start,/open,/close,/history,/conversation"
+COMMANDS: str = "/help,/start,/open,/close,/history,/conversation,/regist,/deregist,/handlers"
